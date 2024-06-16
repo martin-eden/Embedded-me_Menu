aeacb7fd6060a3cbdf3c2c4573e063ae63332e7a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-06-16 08:22:25 +0200

    [!+?] Payload functions for menu items..
    With functions fun begins.
    
    Each menu item has ".Method", which can be ".Run()".
    
    So yes, you don't need ugly "switch" of "if else if" spaghetti in
    your code. Just populate menu in some inconspicuous place and then
    Run() the menu item selected.
    
    Frankly, job of running selected menu item is on menu. I'll add it
    there later.
    
    Now example is ugly but real practical. Probably should move it to
    separate snippet.

--- examples/me_Menu/me_Menu.ino
+++ examples/me_Menu/me_Menu.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-06-04
+  Last mod.: 2024-06-16
 */
 
 #include <me_Menu.h>
@@ -11,9 +11,15 @@
 #include <me_InstallStandardStreams.h>
 #include <me_BaseTypes.h>
 
+using
+  me_BaseTypes::TSint_1,
+  me_BaseTypes::TUint_2;
+
 void setup()
 {
+  delay(4000);
   Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
+  Serial.setTimeout(15);
   InstallStandardStreams();
   printf("[me_MenuItem] We are here.\n");
 
@@ -26,16 +32,98 @@ void loop()
 {
 }
 
-// Forwards:
-void Test_BasicUsage();
+/*
+  Below comes menu demonstration
+
+  I thought about very simple but with it's own state.
+
+  I came with OUTPUT_LED on/off/get-state:
+
+    g - Print last value written
+    t - Set led HIGH
+    c - Set led LOW
+
+  Commands actually strings, they don't need to be characters.
+  But main intention of [me_Menu] is program-to-program channel,
+  think about G codes.
+*/
+
+class TBuiltinLed
+{
+  TSint_1 State = -1; // -1 - unknown, 0 - LOW, 1 - HIGH
+
+  public:
+    TBuiltinLed() { pinMode(LED_BUILTIN, OUTPUT); };
+    void PrintState();
+    void SetLow();
+    void SetHigh();
+};
+
+void TBuiltinLed::PrintState()
+{
+  printf("State(");
+
+  if (State == -1)
+    printf("unknown");
+  else if (State == 0)
+    printf("LOW");
+  else if (State == 1)
+    printf("HIGH");
+  else
+    printf("?"); // WTF?
+
+  printf(")\n");
+}
+
+void TBuiltinLed::SetLow()
+{
+  // LED_BUILTIN is Arduino stock #define, so as LOW and HIGH
+  digitalWrite(LED_BUILTIN, LOW);
+  State = 0;
+}
+
+void TBuiltinLed::SetHigh()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  State = 1;
+}
+
+/*
+  Ugly wrappers for class methods
+
+  Because I did not find a way to get pointer to member function.
+  <TMethodCall> knows nothing about fancy classes it store. And should
+  know nothing. It's job just call zero-arguments method with pointer
+  to data.
+*/
+void PrintState_wrap(TUint_2 Instance)
+{
+  TBuiltinLed * LedManager = (TBuiltinLed *) Instance;
+  LedManager->PrintState();
+}
+
+void SetLow_wrap(TUint_2 Instance)
+{
+  TBuiltinLed * LedManager = (TBuiltinLed *) Instance;
+  LedManager->SetLow();
+}
+
+void SetHigh_wrap(TUint_2 Instance)
+{
+  TBuiltinLed * LedManager = (TBuiltinLed *) Instance;
+  LedManager->SetHigh();
+}
+
+/*
+  Here comes menu creation
+
+  This function never returns, there are no "exit" command.
+*/
 
 void TestMenu()
 {
-  Test_BasicUsage();
-}
+  TBuiltinLed LedManager;
 
-void Test_BasicUsage()
-{
   using
     me_Menu::TMenu,
     me_Menu::TMenuItem;
@@ -44,28 +132,29 @@ void Test_BasicUsage()
 
   TMenuItem Item;
 
-  Item.Command.Set("Put");
-  Item.Description.Set("Put data in range");
+  Item.Command.Set("g");
+  Item.Description.Set("Print last value written");
+  Item.Method.Set((TUint_2) &LedManager, (TUint_2) &PrintState_wrap);
   Menu.Add(&Item);
 
-  Item.Command.Set("Get");
-  Item.Description.Set("Get data in range");
+  Item.Command.Set("c");
+  Item.Description.Set("Set led LOW");
+  Item.Method.Set((TUint_2) &LedManager, (TUint_2) &SetLow_wrap);
   Menu.Add(&Item);
 
-  Item.Command.Set("SetRange");
-  Item.Description.Set("Set memory range");
-  Menu.Add(&Item);
-
-  Item.Command.Set("GetRange");
-  Item.Description.Set("Get memory range");
+  Item.Command.Set("t");
+  Item.Description.Set("Set led HIGH");
+  Item.Method.Set((TUint_2) &LedManager, (TUint_2) &SetHigh_wrap);
   Menu.Add(&Item);
 
   Menu.Print();
 
-  while(!Menu.GetSelection(&Item));
+  while (true)
+  {
+    while(!Menu.GetSelection(&Item));
 
-  Item.Description.Print();
-  printf("\n");
+    Item.Method.Run();
+  }
 }
 
 /*
@@ -75,4 +164,5 @@ void Test_BasicUsage()
   2024-06-01
   2024-06-02
   2024-06-04
+  2024-06-16
 */
