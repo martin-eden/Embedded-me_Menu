3f94bd497a146fa139dc594f1168afe4db08eea7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-12 22:23:50 +0200

    [.] Some code rearrangements for readability..
    I've visited this module while testing work of [me_Heap].
    
    * Code is too scattered now, needs consolidation.
    
    * AddItem(...) is really needed. Every user implements it.
    
    * Descriptions takes most of memory. They are short, but users
      are adding commands till memory exhaust.
    
      Wish to move descriptions for progmem when we'll be ready.
    
    * I think at this stage SpawnItem()/KillItem() can be reimplemented
      in a more clear way.
    
      SpawnItem() doesn't zeroes memory. That was an issue when I plugged
      custom memory manager which doesn't zeroed allocated memory too.
    
      Now I've made memory zeroing part of allocator's contract, but
      I still wish to revise SpanwItem() implementation.

--- src/me_Menu.AddRelease.cpp
+++ src/me_Menu.AddRelease.cpp
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-10-05
+  Last mod.: 2024-10-12
 */
 
 #include "me_Menu.h"
@@ -11,23 +11,25 @@
 
 using
   me_Menu::TMenu,
-  me_Menu::TMenuItem,
-  me_MenuItem::Freetown::SpawnItem,
-  me_MenuItem::Freetown::KillItem;
+  me_Menu::TMenuItem;
 
 /*
   Add menu item to menu
 */
-TBool TMenu::Add(TMenuItem * OuterMenuItem)
+TBool TMenu::Add(
+  TMenuItem * OuterMenuItem
+)
 {
   TMenuItem * Item;
 
-  if (!SpawnItem(&Item, OuterMenuItem))
+  // Create our copy in dynamic memory
+  if (!me_MenuItem::Freetown::SpawnItem(&Item, OuterMenuItem))
     return false;
 
+  // Add our copy to menu list
   if (!List.Add((TUint_2) Item))
   {
-    KillItem(Item);
+    me_MenuItem::Freetown::KillItem(Item);
     return false;
   }
 
@@ -42,7 +44,7 @@ void KillItem_Handler(
   TUint_2 HandlerData __attribute__((unused))
 )
 {
-  KillItem((TMenuItem *) Data);
+  me_MenuItem::Freetown::KillItem((TMenuItem *) Data);
 }
 
 /*
