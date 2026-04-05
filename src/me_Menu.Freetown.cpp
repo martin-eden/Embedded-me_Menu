51f38ad47316f33706211c9f93c359a709d36fd1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-05 17:22:53 +0200

    [^>] [Freetown]

---
+++ src/me_Menu.Freetown.cpp
@@ -0,0 +1,115 @@
+// Menu's utility functions
+
+/*
+  Author: Martin Eden
+  Last mod.: 2026-04-05
+*/
+
+#include <me_Menu.h>
+
+#include <me_BaseTypes.h>
+#include <me_StoredCall.h>
+#include <me_WorkmemTools.h>
+
+using namespace me_Menu;
+
+/*
+  [Handy] Return item structure from arguments
+
+  No heap allocations here.
+*/
+TMenuItem Freetown::ToItem(
+  TAddressSegment Command,
+  TAddressSegment Description,
+  me_StoredCall::TStoredCall Handler
+)
+{
+  TMenuItem Result;
+
+  Result.Command = Command;
+  Result.Description = Description;
+  Result.Handler = Handler;
+
+  return Result;
+}
+
+/*
+  [Handy] Create item from list of values
+
+  Memory allocated here depends of length of ASCIIZs.
+*/
+TMenuItem Freetown::ToItem(
+  const TAsciiz Command,
+  const TAsciiz Description,
+  TMethod Handler,
+  TAddress Instance
+)
+{
+  using
+    me_WorkmemTools::Reserve,
+    me_WorkmemTools::CopyMemTo,
+    me_WorkmemTools::Release,
+    me_StoredCall::Freetown::ToStoredCall;
+
+  TMenuItem Result; // I hope it is zeroed, we can return it early
+
+  /*
+    Allocate memory and copy ASCIIZ. But first we need to describe
+    them as memory segments.
+  */
+  TAddressSegment CommandCopy;
+  TAddressSegment DescriptionCopy;
+  {
+    TAddressSegment CommandOrig =
+      me_WorkmemTools::FromAsciiz(Command);
+
+    TAddressSegment DescriptionOrig =
+      me_WorkmemTools::FromAsciiz(Description);
+
+    if (!Reserve(&CommandCopy, CommandOrig.Size))
+      return Result;
+
+    CopyMemTo(CommandCopy, CommandOrig);
+
+    if (!Reserve(&DescriptionCopy, DescriptionOrig.Size))
+    {
+      Release(&CommandCopy);
+
+      return Result;
+    }
+
+    CopyMemTo(DescriptionCopy, DescriptionOrig);
+  }
+
+  // Using existing shortcut. Because someone should use it
+  return
+    ToItem(
+      CommandCopy,
+      DescriptionCopy,
+      ToStoredCall(Handler, Instance)
+    );
+}
+
+/*
+  Release memory from:
+    * menu item strings (variable)
+    * menu item structure (12 bytes)
+*/
+void Freetown::KillItem(
+  TMenuItem * Item
+)
+{
+  TAddressSegment ItemSeg = M_AsAddrSeg(*Item);
+
+  // Release item strings
+  me_WorkmemTools::Release(&Item->Command);
+  me_WorkmemTools::Release(&Item->Description);
+
+  // Release item structure (12 bytes)
+  me_WorkmemTools::Release(&ItemSeg);
+}
+
+/*
+  2024 # # # # #
+  2026-04-05
+*/
--- src/me_Menu.cpp
+++ src/me_Menu.cpp
@@ -2,19 +2,15 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2026-01-10
+  Last mod.: 2026-04-05
 */
 
 #include <me_Menu.h>
 
 #include <me_BaseTypes.h>
-#include <me_StoredCall.h>
 #include <me_WorkmemTools.h>
 
 using namespace me_Menu;
-
-using
-  me_StoredCall::TStoredCall;
 
 // ( Menu
 
@@ -127,107 +123,8 @@ void TMenuItem::Execute()
 
 // ) Menu item
 
-// ( Freetown
-
-/*
-  [Handy] Return item structure from arguments
-
-  No heap allocations here.
-*/
-TMenuItem Freetown::ToItem(
-  TAddressSegment Command,
-  TAddressSegment Description,
-  TStoredCall Handler
-)
-{
-  TMenuItem Result;
-
-  Result.Command = Command;
-  Result.Description = Description;
-  Result.Handler = Handler;
-
-  return Result;
-}
-
-/*
-  [Handy] Create item from list of values
-
-  Memory allocated here depends of length of ASCIIZs.
-*/
-TMenuItem Freetown::ToItem(
-  const TAsciiz Command,
-  const TAsciiz Description,
-  TMethod Handler,
-  TAddress Instance
-)
-{
-  using
-    me_WorkmemTools::Reserve,
-    me_WorkmemTools::CopyMemTo,
-    me_WorkmemTools::Release,
-    me_StoredCall::Freetown::ToStoredCall;
-
-  TMenuItem Result; // I hope it is zeroed, we can return it early
-
-  /*
-    Allocate memory and copy ASCIIZ. But first we need to describe
-    them as memory segments.
-  */
-  TAddressSegment CommandCopy;
-  TAddressSegment DescriptionCopy;
-  {
-    TAddressSegment CommandOrig =
-      me_WorkmemTools::FromAsciiz(Command);
-
-    TAddressSegment DescriptionOrig =
-      me_WorkmemTools::FromAsciiz(Description);
-
-    if (!Reserve(&CommandCopy, CommandOrig.Size))
-      return Result;
-
-    CopyMemTo(CommandCopy, CommandOrig);
-
-    if (!Reserve(&DescriptionCopy, DescriptionOrig.Size))
-    {
-      Release(&CommandCopy);
-
-      return Result;
-    }
-
-    CopyMemTo(DescriptionCopy, DescriptionOrig);
-  }
-
-  // Using existing shortcut. Because someone should use it
-  return
-    ToItem(
-      CommandCopy,
-      DescriptionCopy,
-      ToStoredCall(Handler, Instance)
-    );
-}
-
-/*
-  Release memory from:
-    * menu item strings (variable)
-    * menu item structure (12 bytes)
-*/
-void Freetown::KillItem(
-  TMenuItem * Item
-)
-{
-  TAddressSegment ItemSeg = M_AsAddrSeg(*Item);
-
-  // Release item strings
-  me_WorkmemTools::Release(&Item->Command);
-  me_WorkmemTools::Release(&Item->Description);
-
-  // Release item structure (12 bytes)
-  me_WorkmemTools::Release(&ItemSeg);
-}
-
-// ) Freetown
-
 /*
   2024 # # # # # # # # # # # # # # #
-  2025-08-29
+  2025 #
+  2026-04-05
 */
